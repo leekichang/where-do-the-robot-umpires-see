bbe85b94192d40d408b4055bdd6af001d26a6908
leekichang <kichang.lee@yonsei.ac.kr>
2024-07-21 15:58:44 +0900

    data merge+split

---
+++ data/data_merge.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+files = ['./data/2021',
+         './data/2022',
+         './data/2023']
+
+for file in files:
+    merged_data = pd.DataFrame()
+    for i in range(5):
+        f_name = f'{file}_{i}.csv'
+        data = pd.read_csv(f_name, encoding='cp949')
+        merged_data = pd.concat([merged_data, data])
+    merged_data.to_csv(f'{file}.csv', index=False, encoding='cp949')
---
+++ data/data_split.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+files = ['./data/2021.csv', './data/2022.csv', './data/2023.csv']
+
+chunk_size = 50000
+for file in files:
+    data = pd.read_csv(file, encoding='cp949')
+    for i in range(len(data) // chunk_size + 1):
+        data[i * chunk_size:(i + 1) * chunk_size].to_csv(file.replace('.csv', f'_{i}.csv'), index=False, encoding='cp949')
+        
